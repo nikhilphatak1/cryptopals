2cc75343d8d8f954a39c07d984cf38f992849709
Nikhil Phatak <nikhilphatak1@gmail.com>
2021-02-15 21:23:37 -0500

    started set 1 challenge 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use std::env;
+
 fn main() {
     println!("crypto pals");
-    let set_number = std::env::args().nth(1).expect("no set number given").parse::<i32>().unwrap();
-    let problem_number = std::env::args().nth(2).expect("no problem number given").parse::<i32>().unwrap();
+    let args: Vec<_> = env::args().collect();
+    let set_number = args[1].parse::<i32>().unwrap_or(-1);
+    let challenge_number = args[2].parse::<i32>().unwrap_or(-1);
+
+    match set_number {
+        1 => match challenge_number {
+            1 => set_1_challenge_1(&args[2..]),
+            _ => panic!("Unsolved or nonexistent problem specified"),
+        },
+        _ => panic!("Unsolved or nonexistent problem specified"),
+    }
 }
 
+fn set_1_challenge_1(args: &[String]) {
+    let input = &args[0];
+    let hex_content = match hex::decode(input) {
+        Ok(content) => content,
+        Err(error) => panic!("Unable to decode input string as hex: {}", error)
+    };
+
+    
+}
